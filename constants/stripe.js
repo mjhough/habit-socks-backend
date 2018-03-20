3f224bbac1e85ed321f93b4717b51be0429b05d8
Matt Hough <matt@hough.tv>
2018-03-20 14:33:44 +1000

    Use config vars instead of logic in constants folder

--- constants/stripe.js
+++ constants/stripe.js
@@ -1,8 +1,6 @@
 const configureStripe = require('stripe');
 
-const STRIPE_SECRET_KEY = process.env.NODE_ENV === 'production'
-  ? process.env.STRIPE_SECRET
-  : process.env.STRIPE_TEST_SECRET;
+const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET;
 
 const stripe = configureStripe(STRIPE_SECRET_KEY);
 
