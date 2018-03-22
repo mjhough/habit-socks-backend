9cacfbd4ef81aa576aebb6c01ded0c554289e456
Matt Hough <matt@hough.tv>
2018-03-22 11:13:52 +1000

    Did nothing

--- server.js
+++ server.js
@@ -4,12 +4,10 @@ const bodyParser = require('body-parser');
 const CORS_WHITELIST = require('./constants/frontend');
 
 const corsOptions = {
-  origin: (origin, callback) => {
-    new Error(origin + CORS_WHITELIST);
+  origin: (origin, callback) =>
     (CORS_WHITELIST.indexOf(origin) !== -1)
       ? callback(null, true)
       : callback(new Error('Not allowed by CORS'))
-  }
 };
 
 const configureServer = app => {
