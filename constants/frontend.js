30b751a67351b4e2ec68de0d9088f32d757fd48c
Matt Hough <matt@hough.tv>
2018-03-24 09:03:14 +1000

    Try heroku url

--- constants/frontend.js
+++ constants/frontend.js
@@ -2,7 +2,8 @@ const FRONTEND_DEV_URLS = [ 'http://localhost:5000' ];
 
 const FRONTEND_PROD_URLS = [
   'https://www.habitsocks.com',
-  'https://habitsocks.com'
+  'https://habitsocks.com',
+  'https://habit-socks-production.herokuapp.com'
 ];
 
 module.exports = process.env.NODE_ENV === 'production'
